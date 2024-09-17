b662d77d0d7d9f3077fc2abf222603a76f6ee4ba
Joan Nyambura <njugunajoan35@gmail.com>
2024-09-17 19:40:20 +0300

    Refactor: Exchange Rates in the Exchange Info View

---
+++ LOTRConverter17/ExchangeRate.swift
@@ -0,0 +1,32 @@
+//
+//  ExchangeRate.swift
+//  LOTRConverter17
+//
+//  Created by Jojo on 17/09/2024.
+//
+
+import SwiftUI
+
+struct ExchangeRate: View {
+    let leftImage: ImageResource
+    let text: String
+    let rightImage: ImageResource
+    
+    var body: some View {
+        HStack {
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            Text(text)
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+        }
+    }
+}
+
+#Preview {
+    ExchangeRate(leftImage: .silverpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .silverpenny)
+}
